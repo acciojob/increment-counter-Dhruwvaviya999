25098b344ef26ee4ff25d953c0e12851f51e3065
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-22 17:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let count = document.getElementById("counter");
+let incrementBtn = document.getElementById("incrementBtn");
+
+incrementBtn.addEventListener("click", ()=>{
+	alert(count.textContent);
+	count.textContent = parseInt(count.textContent) + 1;
+})
